a7ef8798d51fbe35871566f755c06f1f7c0c255d
AngelCh415! <angelchavezmorones@gmail.com>
2023-03-08 14:07:57 -0600

    Primer inciso

---
+++ Tarea 1/ServidorA.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.*;
+
+public class ServidorA {
+    public static void main(String[] args) throws IOException {
+        ServerSocket servidor = new ServerSocket(12345); // puerto del servidor
+        System.out.println("Servidor A en linea");
+        while (true) {
+            Socket cliente = servidor.accept();
+            System.out.println("Nuevo cliente conectado: " + cliente.getInetAddress().getHostAddress());
+            new ManejadorCliente(cliente).start(); // crear hilo para el nuevo cliente
+        }
+    }
+}
+
+class ManejadorCliente extends Thread {
+    private Socket cliente;
+    
+    public ManejadorCliente(Socket cliente) {
+        this.cliente = cliente;
+    }
+    
+    public void run() {
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            PrintWriter salida = new PrintWriter(new OutputStreamWriter(cliente.getOutputStream()), true);
+            
+            // recibir los 3 numeros del cliente
+            int numero = Integer.parseInt(entrada.readLine());
+            int numeroInicial = Integer.parseInt(entrada.readLine());
+            int numeroFinal = Integer.parseInt(entrada.readLine());
+            
+            boolean divide = false;
+            for (int i = numeroInicial; i <= numeroFinal; i++) {
+                if (numero % i == 0) {
+                    divide = true;
+                    break;
+                }
+            }
+            
+            // enviar respuesta al cliente
+            if (divide) {
+                salida.println("DIVIDE");
+            } else {
+                salida.println("NO DIVIDE");
+            }
+            
+            cliente.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
